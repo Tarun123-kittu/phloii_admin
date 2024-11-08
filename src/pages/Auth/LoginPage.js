fbd22bf7478d4b5578026d539a3db76bf433269a
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2024-11-08 18:57:11 +0530

    button change

--- src/pages/Auth/LoginPage.js
+++ src/pages/Auth/LoginPage.js
@@ -6,7 +6,7 @@ const LoginPage = () => {
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <img
                     className="mx-auto h-10 w-auto"
-                    src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                    src="/logo.png"
                     alt="Your Company"
                 />
                 <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
@@ -58,7 +58,7 @@ const LoginPage = () => {
                     <div>
                         <button
                             type="submit"
-                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            className="flex w-full justify-center rounded-md bg-[#2febfe] px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
                             Sign in
                         </button>
